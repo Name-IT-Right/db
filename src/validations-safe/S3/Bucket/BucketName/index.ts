d3e088a95e25f8ea6004785e90ed3bfe6273fa33
Piotrek Witkowski <piotrekwitkowski1@gmail.com>
2023-02-17 13:33:31 +0000

    Add cast to Validation

--- src/validations-safe/S3/Bucket/BucketName/index.ts
+++ src/validations-safe/S3/Bucket/BucketName/index.ts
@@ -1,3 +1,4 @@
+import { Validation } from "@name-it-right/types";
 import { rawDB } from "../../../../db"
 
-export const BucketName = rawDB["AWS::S3::Bucket"].properties.BucketName.validation;
+export const BucketName = rawDB["AWS::S3::Bucket"].properties.BucketName.validation as Validation;
--- src/validations-safe/SageMaker/NotebookInstance/NotebookInstanceName/index.ts
+++ src/validations-safe/SageMaker/NotebookInstance/NotebookInstanceName/index.ts
@@ -1,3 +1,4 @@
-import { rawDB } from "../../../../db"
+import { Validation } from "@name-it-right/types";
+import { rawDB } from "../../../../db";
 
-export const NotebookInstanceName = rawDB["AWS::SageMaker::NotebookInstance"].properties.NotebookInstanceName.validation;
+export const NotebookInstanceName = rawDB["AWS::SageMaker::NotebookInstance"].properties.NotebookInstanceName.validation as Validation;
